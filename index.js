83779c23bbca107860999683061ee25446868839
Daniel Yim <daniel.yim96@gmail.com>
2022-11-21 00:16:43 -0500

    commit tree 266c0d8594a9c2b115d72c27498f34badfaf0313

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
 
-// Write your code here
+let random = Math.ceil(Math.random());
+let num3 = 36;
+let num4 = 8;
+let mod = (num3 % num4);
+
+let max = Math.max(1,2,3,4,20);
